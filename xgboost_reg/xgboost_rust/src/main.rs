8cb22955d0f305c4e57263de00d09b5368f7a4b4
GaryHU <swe1609011@gmail.com>
2023-09-20 01:25:50 +0800

    fixed parsing 0/1 bug in sexpr output from egg; update data collection script for xgboost

--- xgboost_reg/xgboost_rust/src/main.rs
+++ xgboost_reg/xgboost_rust/src/main.rs
@@ -4,14 +4,16 @@ use std::io::BufReader;
 use xgboost::{DMatrix, Booster, parameters};
 use csv;
 use rand::seq::SliceRandom;
+use xgboost::parameters::learning::EvaluationMetric;
 
 
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut best_rmse = f32::INFINITY;
+    let mut best_mae = f32::INFINITY;
     let mut best_model: Option<Booster> = None;
 
-    let file = File::open("/data/guangyuh/coding_env/E-Brush/xgboost_reg/data_5000.csv")?;
+    let file = File::open("/data/guangyuh/coding_env/E-Brush/xgboost_reg/data_10000_area.csv")?;
     let mut reader = csv::Reader::from_reader(BufReader::new(file));
 
     let mut data = Vec::new();
@@ -34,6 +36,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     data.shuffle(&mut rng);
 
     let mut avg_rmse = 0.0;
+    let mut avg_mae = 0.0;
 
     for fold in 0..num_folds {
         let test_start = fold * fold_size;
@@ -63,6 +66,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         // configure objectives, metrics, etc.
         let learning_params = parameters::learning::LearningTaskParametersBuilder::default()
             .objective(parameters::learning::Objective::GpuRegLinear)
+            .eval_metrics(parameters::learning::Metrics::Custom(vec![EvaluationMetric::MAE]))
+            //.eval_metrics(parameters::learning::Metrics::Auto)
             .build().unwrap();
 
         // configure the tree-based learning model's parameters
@@ -98,17 +103,26 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         let preds = bst.predict(&dtest).unwrap();
         let rmse = (y_test.iter().zip(preds.iter()).map(|(y, y_hat)| (y - y_hat).powi(2)).sum::<f32>() / y_test.len() as f32).sqrt();
+        let mae = (y_test.iter().zip(preds.iter()).map(|(y, y_hat)| (y - y_hat).abs()).sum::<f32>() / y_test.len() as f32);
         println!("Fold {}: RMSE = {}", fold + 1, rmse);
+        println!("Fold {}: MAE = {}", fold + 1, mae);
         avg_rmse += rmse;
+        avg_mae += mae;
 
-        if rmse < best_rmse {
-            best_rmse = rmse;
+        // if rmse < best_rmse {
+        //     best_rmse = rmse;
+        //     best_model = Some(bst);
+        // }
+        if mae < best_mae {
+            best_mae = mae;
             best_model = Some(bst);
         }
     }
 
     avg_rmse /= num_folds as f32;
+    avg_mae /= num_folds as f32;
     println!("Average RMSE: {}", avg_rmse);
+    println!("Average MAE: {}", avg_mae);
     // Save the best model
     if let Some(best_model) = best_model {
         best_model.save("xgb.model")?;
@@ -116,9 +130,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // load model and predict
     let bst = Booster::load("xgb.model")?;
-    let x_test = &[0.0,73.0,79.0,232.0,14.0,2395.0,152.0,15.756578947368421];
+    //let x_test = &[0.0,73.0,79.0,232.0,14.0,2395.0,152.0,15.756578947368421]; //test delay
+    let x_test = &[7.0,101.0,88.0,316.0,15.0,3027.0,208.0,15.443877551020408];//test area
     let num_rows = 1;
-    let y_test = &[53.05];
+   //let y_test = &[53.05]; //test delay
+    let y_test = &[67.65]; //test area
     let mut dtest = DMatrix::from_dense(x_test, num_rows).unwrap();
     dtest.set_labels(y_test).unwrap();
     println!("Prediction: {:?}", bst.predict(&dtest).unwrap());
