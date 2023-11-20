fec63087402ff3a5874913278e514c668c5e1535
GaryHU <swe1609011@gmail.com>
2023-11-20 16:59:47 +0800

    update data visulization

--- xgboost_reg/model_comparision.py
+++ xgboost_reg/model_comparision.py
@@ -13,46 +13,48 @@ data = pd.read_csv('/data/guangyuh/coding_env/E-Brush/xgboost_reg/collect_datase
 X = data.iloc[:, :8].values
 # y is area * 0.4 + delay * 0.6
 #y = (0.4 * data['area'] + 0.6 * data['delay'])
-y = data['delay'].values
+y = data['area'].values
 
 def cross_val_evaluate(model, X, y, n_folds=10):
     mae_scores = -cross_val_score(model, X, y, cv=n_folds, scoring='neg_mean_absolute_error')
+    mape = cross_val_score(model, X, y, scoring='neg_mean_absolute_percentage_error', cv=n_folds)
     rrse_scores = np.sqrt(-cross_val_score(model, X, y, cv=n_folds, scoring='neg_mean_squared_error')) / np.std(y)
     r_scores = cross_val_score(model, X, y, cv=n_folds, scoring='r2')
-    return mae_scores.mean(), rrse_scores.mean(), r_scores.mean()
+    rmse_scores = np.sqrt(-cross_val_score(model, X, y, cv=n_folds, scoring='neg_mean_squared_error'))
+    return mae_scores.mean(), rrse_scores.mean(), r_scores.mean(), mape.mean(), rmse_scores.mean()
 
 # Linear Regression
 lr = LinearRegression()
-lr_mae, lr_rrse, lr_r = cross_val_evaluate(lr, X, y)
+lr_mae, lr_rrse, lr_r , lr_mape, lr_rmse = cross_val_evaluate(lr, X, y)
 
 # Decision Tree
 dt = DecisionTreeRegressor()
-dt_mae, dt_rrse, dt_r = cross_val_evaluate(dt, X, y)
+dt_mae, dt_rrse, dt_r , dt_mape, dt_rmse = cross_val_evaluate(dt, X, y)
 
 # Random Forest
 rf = RandomForestRegressor()
-rf_mae, rf_rrse, rf_r = cross_val_evaluate(rf, X, y)
+rf_mae, rf_rrse, rf_r, rf_mape, rf_rmse = cross_val_evaluate(rf, X, y)
 
 # Ridge Regression
 ridge = Ridge()
-ridge_mae, ridge_rrse, ridge_r = cross_val_evaluate(ridge, X, y)
+ridge_mae, ridge_rrse, ridge_r , ridge_mape, ridge_rmse = cross_val_evaluate(ridge, X, y)
 
 # Lasso Regression
 lasso = Lasso()
-lasso_mae, lasso_rrse, lasso_r = cross_val_evaluate(lasso, X, y)
+lasso_mae, lasso_rrse, lasso_r , lasso_mape, lasso_rmse = cross_val_evaluate(lasso, X, y)
 
 # Elastic Net Regression
 elastic_net = ElasticNet()
-elastic_net_mae, elastic_net_rrse, elastic_net_r = cross_val_evaluate(elastic_net, X, y)
+elastic_net_mae, elastic_net_rrse, elastic_net_r , elastic_net_mape, elastic_net_rmse = cross_val_evaluate(elastic_net, X, y)
 
 # KNN Regression
 knn = KNeighborsRegressor()
-knn_mae, knn_rrse, knn_r = cross_val_evaluate(knn, X, y)
+knn_mae, knn_rrse, knn_r, knn_mape, knn_rmse = cross_val_evaluate(knn, X, y)
 
-print("Linear Regression: MAE={}, RRSE={}, R={}".format(lr_mae, lr_rrse, lr_r))
-print("Decision Tree: MAE={}, RRSE={}, R={}".format(dt_mae, dt_rrse, dt_r))
-print("Random Forest: MAE={}, RRSE={}, R={}".format(rf_mae, rf_rrse, rf_r))
-print("Ridge Regression: MAE={}, RRSE={}, R={}".format(ridge_mae, ridge_rrse, ridge_r))
-print("Lasso Regression: MAE={}, RRSE={}, R={}".format(lasso_mae, lasso_rrse, lasso_r))
-print("Elastic Net Regression: MAE={}, RRSE={}, R={}".format(elastic_net_mae, elastic_net_rrse, elastic_net_r))
-print("KNN Regression: MAE={}, RRSE={}, R={}".format(knn_mae, knn_rrse, knn_r))
+print("Linear Regression: MAE={}, RRSE={}, R_square={}, MAPE={}, RMSE={}".format(lr_mae, lr_rrse, lr_r,lr_mape, lr_rmse))
+print("Decision Tree: MAE={}, RRSE={}, R_square={}, MAPE={}, RMSE = {}".format(dt_mae, dt_rrse, dt_r,dt_mape, dt_rmse))
+print("Random Forest: MAE={}, RRSE={}, R_square={}, MAPE={}, RMSE = {}".format(rf_mae, rf_rrse, rf_r,rf_mape, rf_rmse))
+print("Ridge Regression: MAE={}, RRSE={}, R_square={}, MAPE={}, RMSE = {}".format(ridge_mae, ridge_rrse, ridge_r, ridge_mape, ridge_rmse))
+print("Lasso Regression: MAE={}, RRSE={}, R_square={}, MAPE={}, RMSE = {}".format(lasso_mae, lasso_rrse, lasso_r,lasso_mape, lasso_rmse))
+print("Elastic Net Regression: MAE={}, RRSE={}, R_square={}, MAPE={}, RMSE = {}".format(elastic_net_mae, elastic_net_rrse, elastic_net_r,elastic_net_mape, elastic_net_rmse))
+print("KNN Regression: MAE={}, RRSE={}, R_square={}, MAPE={}, RMSE = {}".format(knn_mae, knn_rrse, knn_r, knn_mape, knn_rmse))
--- xgboost_reg/train.py
+++ xgboost_reg/train.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 from sklearn import metrics
 import m2cgen as m2c
+from sklearn.inspection import permutation_importance
 
 def mape(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
@@ -17,11 +18,11 @@ def rrse(y_true, y_pred):
 def r(y_true, y_pred):
     return np.corrcoef(y_true, y_pred)[0, 1]
 
-df = pd.read_csv('collect_dataset/fuzz_circuit_analysis_merge_size_51000_23_10_26.csv')
+df = pd.read_csv('/data/guangyuh/coding_env/E-Brush/xgboost_reg/collect_dataset/fuzz_circuit_analysis_merge_size_51000_23_10_26.csv')
 X = df.iloc[:, :8].values
-#y = ( df['area'] * df['delay']).values
-y = df['delay'].values
-
+#y = ( 0.4 * df['area'] + 0.6 * df['delay']).values
+#y = df['delay'].values
+y = df['area'].values
 # Scale the features
 # scaler = StandardScaler()
 # X = scaler.fit_transform(X)
@@ -46,7 +47,7 @@ kf = KFold(n_splits=10, shuffle=True, random_state=0)
 # print data size
 print("X_train size:", X_train.shape)
 print("X_test size:", X_test.shape)
-grid_search = GridSearchCV(estimator=model, param_grid=params, scoring='neg_mean_absolute_error', cv=kf, verbose=1)
+grid_search = GridSearchCV(estimator=model, param_grid=params, scoring='neg_mean_absolute_percentage_error', cv=kf, verbose=1)
 grid_search.fit(X,y)
 
 best_params = grid_search.best_params_
@@ -57,15 +58,44 @@ model_full = xgb.XGBRegressor(**best_params).fit(X_train, y_train)
 plot_importance(model_full)
 plt.savefig('feature_importance.png')
 
+# Performing permutation importance
+result = permutation_importance(
+    model_full, X_train, y_train, n_repeats=10, random_state=42, n_jobs=2
+)
+
+# Sorting importances
+sorted_importances_idx = result.importances_mean.argsort()
+#feature_names = list(df.columns)
+#df_columns_sorted = [feature_names[i] for i in sorted_importances_idx]
+
+# Creating DataFrame for plotting
+importances = pd.DataFrame(
+    result.importances[sorted_importances_idx].T,
+    columns=X.columns[sorted_importances_idx],
+)
+
+# Plotting the permutation importances
+ax = importances.plot.box(vert=False, whis=10)
+ax.set_title("Permutation Importances (train set)")
+ax.axvline(x=0, color="k", linestyle="--")
+ax.set_xlabel("Decrease in accuracy score")
+fig = ax.get_figure()
+fig.tight_layout()
+fig.savefig('permutation_importance.png')
+
 # Print the best score (mean absolute error)
-best_score = -grid_search.best_score_
-print("Best Mean Absolute Error:", best_score)
+best_mape_score = -grid_search.best_score_
+print("Best Mean Percentage Absolute Error:", best_mape_score)
+
+#best_mape_score = -grid_search.best_score_
 
 y_pred = model_full.predict(X_test)
-print("Mean Absolute Error Percentage (MAPE):", mape(y_test, y_pred))
+print("Mean Absolute Error Percentage (MAPE):", metrics.mean_absolute_percentage_error(y_test, y_pred))
 print("Root Relative Square Error (RRSE):", rrse(y_test, y_pred))
 print("Correlation Coefficient (R):", r(y_test, y_pred))
+print("Coeff Determination (R^2):", metrics.r2_score(y_test, y_pred))
 print("Mean Absolute Error (MAE):", metrics.mean_absolute_error(y_test, y_pred))
+print("RMSE (Root Mean Squared Error):", np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 
 code = m2c.export_to_rust(model_full)
 
